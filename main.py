081794754984caefd780c5f3daa4dbfd9720901e
Nikolay Vyahhi <nikolay.vyahhi@hyperskill.org>
2024-11-03 21:13:46 -0500

    video+audio, works only locally

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import random
 import requests
 import time
 import uvicorn
@@ -75,7 +76,7 @@ async def generate_music_form(request: Request):
     for audio in audios:
         url = audio["url"]
         idd = audio["id"]
-        html += f"<a href='{url}' target='_blank'>Listen</a>, <a href='/videofy/{idd}' target='_blank'>Videofy</a>, ID: {idd}<br>"
+        html += f"<a href='{url}' target='_blank'>Listen</a>, <a href='/videofy/{idd}/NO' target='_blank'>Videofy</a>, ID: {idd}<br>"
     html += f"<h1>Lyrics:</h1><pre>{lyrics}</pre>"
     html += f"<h1>Style:</h1><pre>{style}</pre>"
     html += f"<h1>User Query:</h1><pre>{query}</pre>"
@@ -98,10 +99,68 @@ async def generate_music_api(request: Request, input: dict):
     )
 
 
-@app.get("/videofy/{suno_id}")
-async def videofy(suno_id: str):
+VIDEO_URLS = """
+iFiPxTrcoWw
+wv8_ePzdMv8
+qK1VjY_cU9w
+13_4cPyWiIo
+dBE0pZtK3ao
+GA8vYmmvqEk
+oPz7Uh_6ey4
+r5utBFtLtWk
+3j5PUUQz5cw
+Pap_Ln-Fz2A
+0ikEJppc9qQ
+FHkeRqGnNQk
+A2RQGBQvHfI
+IWy5lAkt6CY
+CCfNnjiN8xI
+iUEccNy3m04
+l0aysU5iE5k
+wrSXoFBozu4
+ckKNrq56XIw
+-IegPHSc2VA
+o9b2oynTQXo
+X_79-x_nLzs
+1e6Rn6JEICc
+StgZ5ct4Jx4
+QZQPRIg245A
+vC04Xj7NWBc
+Pp7fmvfyaOI
+YB10EbfzZts
+mvNSGMcTN1s
+pSgWt_CFtHM
+i5w6Y74ZgYk
+FXzWKqMPHI0
+prmMgmdM-xc
+xKRNDalWE-E
+rsEP9N9c5CQ
+bb07ui130-8
+7cIPxrLYw8M
+--owd7CIjYs
+axAYvo8gOIA
+B2GM98bKhVg
+E1CgDCh5KC0
+BbM2MJ6aeZE
+rYPeM4m-tJc
+oSzKvHzjrSA
+YpjTpjFuhZM
+wXN08vl6TWI
+AGnZ8nMnbv4
+lzEEhDRRafM
+Ctdg-sOW8po
+6GPXGBg7UGo
+4ASNL5RwghA
+lVtPEAeM-UM
+cOUcu-xSKHM""".strip().splitlines()
+
+
+@app.get("/videofy/{suno_id}/{yt_id}")
+async def videofy(suno_id: str, yt_id: str = None):
+    if yt_id == "NO":
+        yt_id = random.choice(VIDEO_URLS)
     os.makedirs("files", exist_ok=True)
-    print(suno_id)
+    print(suno_id, yt_id)
     audio_url = None
     for _ in range(60):
         data = get_audio_information(suno_id)
@@ -118,34 +177,24 @@ async def videofy(suno_id: str):
         with open(audio_filename, "wb") as file:
             file.write(response.content)
 
-    YTID = "xKRNDalWE-E"
+    video_filename = f"files/{yt_id}.mp4"
     ydl_opts = {
         "format": "bestvideo",
-        "outtmpl": "files/%(id)s.%(ext)s",  # Specify the output template
+        "outtmpl": video_filename,
     }
 
     with YoutubeDL(ydl_opts) as ydl:
-        urls = f"https://www.youtube.com/watch?v={YTID}"
+        urls = f"https://www.youtube.com/watch?v={yt_id}"
         ydl.download(urls)
 
-    video_filename = f"{YTID}.webm"
-
-    # stream = ffmpeg.input("input.mp4")
-    # stream = ffmpeg.hflip(stream)
-    # stream = ffmpeg.output(stream, "output.mp4")
-    # ffmpeg.run(stream)
-
-    # video_filename = "video.mp4"  # Assuming the video file is named "video.mp4"
-    # output_filename = "merged_video.mp4"  # The output filename for the merged video
-
-    # # Command to merge video and audio using ffmpeg
     output_filename = f"files/merged-{suno_id}.mp4"
+    download_filename = f"tune-tutor-{suno_id}.mp4"
     merge_command = f'ffmpeg -y -i "{video_filename}" -stream_loop -1 -i "{audio_filename}" -map 0:v -map 1:a -c:v copy -shortest {output_filename}'
     print(merge_command)
     os.system(merge_command)
 
     return FileResponse(
-        output_filename, media_type="video/mp4", filename=output_filename
+        output_filename, media_type="video/mp4", filename=download_filename
     )
 
 
